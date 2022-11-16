6e5967c1075abf74d306b5103e839a0dd5e49d20
Anders Bille Lind <anders.bille.lind@gmail.com>
2022-11-16 15:05:48 +0100

    fix: prevention of weird translate on last to first

--- ecommerce-product-page/src/components/Slider.tsx
+++ ecommerce-product-page/src/components/Slider.tsx
@@ -29,10 +29,10 @@ export default ({ slides }: SlidesProp) => {
 
 	return (
 		<section className="w-full h-60 relative">
-			<button onClick={prevSlide} className={`absolute left-0 inset-y-1/2 translate-t-1/2 z-20 bg-white rounded-full w-6 h-6 inline-flex items-center justify-center ${current ===0 ? 'hidden': ''}`}>
+			<button onClick={prevSlide} className={`absolute left-0 ml-1 inset-y-1/2 translate-t-1/2 z-20 bg-white rounded-full w-6 h-6 inline-flex items-center justify-center ${current ===0 ? 'hidden': ''}`}>
 				<IconPrevious className="-ml-1" />
 			</button>
-			<button onClick={nextSlide} className={`absolute right-0 top-1/2 translate-t-1/2 z-20 bg-white rounded-full w-6 h-6 inline-flex items-center justify-center ${current ===length-1 ? 'hidden': ''}`}>
+			<button onClick={nextSlide} className={`absolute right-0 mr-1 top-1/2 translate-t-1/2 z-20 bg-white rounded-full w-6 h-6 inline-flex items-center justify-center ${current ===length-1 ? 'hidden': ''}`}>
 				<IconNext className="-mr-1" />
 			</button>
 			<div className="relative overflow-hidden w-full h-full">
