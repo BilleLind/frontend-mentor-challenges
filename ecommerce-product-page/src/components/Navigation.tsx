85df957e0f64092337a11525477f6125d6763379
Anders Bille Lind <anders.bille.lind@gmail.com>
2022-11-22 10:09:37 +0100

    feat: better navigation

--- ecommerce-product-page/src/components/Navigation.tsx
+++ ecommerce-product-page/src/components/Navigation.tsx
@@ -1,13 +1,21 @@
+import { useState } from 'react'
+import Cart from './Cart'
 import { IconCart, IconClose, IconLogo, IconMenu } from './SVG'
 
 export default ({ open, openMenu }: { open: boolean; openMenu: () => void }) => {
+	const [showCart, setShowCart] = useState(false)
+	const openCart = () => {
+		setShowCart((pre) => !pre)
+	}
 	return (
 		<header className="flex justify-between items-center relative z-30 border-b border-lightGrayishBlue text-lg">
 			<nav className="flex items-center justify-center p-4 gap-x-4">
-				<IconMenu onClick={openMenu} className=" md:hidden" />
+				<button onClick={openMenu} className="outline-none group md:hidden" title="Navigation Menu">
+					<IconMenu className="fill-veryDarkBlue transition duration-300 transform group-hover:fill-orange group-hover:-rotate-12 group-focus:fill-orange group-focus:-rotate-12" />
+				</button>
 				<div className={`${open ? 'absolute top-0 left-0 w-full h-screen bg-black/75 z-30' : 'hidden'}`}>
 					<div className="bg-white p-4 w-3/5 h-screen flex flex-col">
-						<button onClick={openMenu} className="mb-4 group outline-none">
+						<button onClick={openMenu} className="mb-4 group outline-none" title="Close Navigation Menu">
 							<IconClose className="fill-veryDarkBlue transition duration-300 transform group-hover:rotate-90 group-hover:scale-105 group-hover:fill-orange group-focus:scale-105 group-focus:rotate-90 group-focus:fill-orange outline-none" />
 						</button>
 
@@ -29,17 +37,23 @@ export default ({ open, openMenu }: { open: boolean; openMenu: () => void }) => 
 						</a>
 					</div>
 				</div>
-				<a href="#">
-					<IconLogo />
+				<a href="/" className="outline-none group" title="Home page">
+					<IconLogo className="text-current transition duration-300 group-hover:fill-orange" />
 				</a>
 			</nav>
-			<div className="p-2 flex gap-x-2">
-				<button>
-					<IconCart />
+			<div className="p-2 flex gap-x-2 relative">
+				<button onClick={openCart} className="group outline-none" title="Shopping Cart">
+					<IconCart className={`transition duration-300 transform group-hover:fill-orange group-hover:-rotate-12 group-focus:fill-orange group-focus:-rotate-12 `} />{' '}
+					{/* ${showCart ? '-rotate-12 fill-orange': ''} */}
 				</button>
-				<button className="h-6 w-6">
-					<img src="./images/image-avatar.png" alt="" />
+				<button className="h-6 w-6 outline-none group" title="Profile">
+					<img
+						src="./images/image-avatar.png"
+						alt=""
+						className="transition duration-300 transform group-hover:scale-125 group-focus:scale-125 rounded-full group-hover:border-orange group-hover:border-2 group-focus:border-2 group-focus:border-orange"
+					/>
 				</button>
+				{showCart ? <Cart /> : null}
 			</div>
 		</header>
 	)
